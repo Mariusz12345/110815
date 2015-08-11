96e68510c0939f9e88a7641d9f44cb3bdb1e7016
Mariusz12345 <przybylowski303@gmail.com>
2015-08-11 07:43:19 +0200

    PierwszyHQL

--- PierwszyHQL/src/test/hql/HibernateTest.java
+++ PierwszyHQL/src/test/hql/HibernateTest.java
@@ -10,17 +10,35 @@ public class HibernateTest {
 
 	public static void main(String[] args) {
 		UzytkownikDane dane = new UzytkownikDane();
-		dane.setUserID(5);
-		dane.setUserName("inyy");
+		UzytkownikDane dane2 = new UzytkownikDane();
+		
+		dane.setUserName("Jan");
 		dane.setDataDolaczenia(new Date());
+		
+		dane2.setUserName("Piotr");
+		dane2.setDataDolaczenia(new Date());
+		
 		
 		SessionFactory sessionfactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionfactory.openSession();
 		session.beginTransaction();
 		session.save(dane);
+		session.save(dane2);
 		session.getTransaction().commit();
+		
+		
+			//odzyskywanie obiektu za pomoca session.get
+//		dane = null;
+//		
+//		 session = sessionfactory.openSession();
+//		 session.beginTransaction();
+//		 dane = (UzytkownikDane) session.get(UzytkownikDane.class, 1);
+//		 System.out.println("Dane uzytkownika: " + dane.getUserName());
+		
+		
+		
+		
 		session.close();
 		sessionfactory.close();
 	}
-
 }
--- PierwszyHQL/src/test/hql/UzytkownikDane.java
+++ PierwszyHQL/src/test/hql/UzytkownikDane.java
@@ -4,18 +4,25 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="DaneUzytkownika")
+@Table(name="DaneUzytkownika",schema = "test7")
 public class UzytkownikDane {
 	
 	@Id
-	@Column(name = "UserID")
+	@SequenceGenerator(name = "userName_seq", sequenceName = "test7.userName_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userName_seq")
 	private int userID;
+	
 	@Column(name ="UserName")
 	private String userName;
+	
+	@Column(name = "DataDolaczenia")
 	private Date dataDolaczenia;
 	
 	public Date getDataDolaczenia() {
